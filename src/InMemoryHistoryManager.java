f730221454f836984e727029440a5cec4d0039d3
VeraBes <bespalaya.vera@gmail.com>
2025-09-15 09:31:33 +0300

    five sprint

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,20 +1,82 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class InMemoryHistoryManager implements HistoryManager{
-    private int maxSize = 5;
+public class InMemoryHistoryManager implements HistoryManager {
+    private final int maxSize = 10;
 
-    private List<Task> historyTasks = new ArrayList<>(maxSize);
+    private final Map<Integer, Node<Task>> historyTasks = new HashMap<>();
+
+    private final CustomLinkedList<Task> customLinkedList = new CustomLinkedList<>();
+
     @Override
     public void add(Task task) {
-        if (historyTasks.size()==maxSize) {
-            historyTasks.remove(0);
+        if (historyTasks.containsKey(task.getId())) {
+            remove(task.getId());
         }
-        historyTasks.add(task);
+        customLinkedList.linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return historyTasks;
+        return customLinkedList.getTasks();
+    }
+
+    @Override
+    public void remove(int id) {
+        customLinkedList.removeNode(historyTasks.get(id));
+        historyTasks.remove(id);
+    }
+
+    class CustomLinkedList<T> {
+        public Node<Task> head;
+        public Node<Task> tail;
+        public int size = 0;
+
+        public void linkLast(Task task) {
+            final Node<Task> oldTail = tail;
+            final Node<Task> newNode = new Node<>(tail, task, null);
+            tail = newNode;
+            if (oldTail == null) {
+                head = newNode;
+            } else {
+                oldTail.setNext(newNode);
+            }
+
+            size++;
+            historyTasks.put(task.getId(), newNode);
+        }
+
+        public List<Task> getTasks() {
+            ArrayList<Task> arrayListTasks = new ArrayList<>();
+            Node<Task> currentNode = head;
+            while (currentNode != null) {
+                arrayListTasks.add(currentNode.getData());
+                currentNode = currentNode.getNext();
+            }
+            return arrayListTasks;
+        }
+
+        public void removeNode(Node<Task> node) {
+            if (node == head) {
+                head = node.getNext();
+                head.setPrev(null);
+                node.setNext(null);
+            } else if (node == tail) {
+                tail = node.getPrev();
+                tail.setNext(null);
+                node.setPrev(null);
+            } else {
+                Node<Task> prevNode = node.getPrev();
+                Node<Task> nextNode = node.getNext();
+
+                if (prevNode != null) {
+                    prevNode.setNext(nextNode);
+                }
+                if (nextNode != null) {
+                    nextNode.setPrev(prevNode);
+                }
+                node.setPrev(null);
+                node.setNext(null);
+            }
+        }
     }
 }
--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,27 +1,34 @@
 public class TaskTracker {
     public static void main(String[] args) {
         TaskManager manager = Managers.getDefault();
-        Task task1 = new Task("забрать заказ", "заказ №125");
-        Task task2 = new Task("сложить вещи в шкаф", "вещи должны лежать на своих полках");
-        Epic epic1 = new Epic("купить дом", "дом должен быть удобный и красивый");
-        Subtask subtask1 = new Subtask("посмотреть объявления о продаже", "объявления 21212", epic1.getId());
-        Subtask subtask2 = new Subtask("выбрать расположение", "расположение в деревне", epic1.getId());
-        Epic epic2 = new Epic("ораганизовать праздник", "день рождения");
-        Subtask subtask3 = new Subtask("заказать торт", "торт большой, вкусный", epic2.getId());
+        Task task1 = new Task("task1", "Забрать заказ №125");
+        Task task2 = new Task("task2", "Сложить вещи в шкафу");
+        Epic epic1 = new Epic("epic1 ", "купить дом. должен быть удобный и красивый");
+        Subtask subtask1 = new Subtask("subtask1 (относится к epic1)", "посмотреть объявления о продаже", epic1.getId());
+        Subtask subtask2 = new Subtask("subtask2 (относится к epic1)", "выбрать расположение дома", epic1.getId());
+        Subtask subtask4 = new Subtask("subtask4 (относится к epic1)", "сьездить на просмотр на машине", epic1.getId());
+        Epic epic3 = new Epic("epic3", "Выучить уроки история и математика");
+        Epic epic2 = new Epic("epic2", "ораганизовать праздник день рождения");
+        Subtask subtask3 = new Subtask("subtask3 (относится к epic2)", "заказать торт ко дню рождения", epic2.getId());
 
         manager.createTask(task1);
         manager.createTask(task2);
         manager.createEpicTask(epic1);
+
         manager.createSubTask(subtask1, epic1.getId());
         manager.createSubTask(subtask2, epic1.getId());
+        manager.createSubTask(subtask4, epic1.getId());
         manager.createEpicTask(epic2);
         manager.createSubTask(subtask3, epic2.getId());
+        manager.createEpicTask(epic3);
+
 
         System.out.println("task1= " + task1);
         System.out.println("task2= " + task2);
         System.out.println("epic1= " + epic1);
         System.out.println("subtask1= " + subtask1);
         System.out.println("subtask2= " + subtask2);
+        System.out.println("subtask4= " + subtask4);
         System.out.println("epic2= " + epic2);
         System.out.println("subtask3= " + subtask3);
        // System.out.println("История просмотров" + manager.getHistory());
@@ -38,12 +45,18 @@ public class TaskTracker {
         System.out.println(manager.getSubTask(subtask1.getId()));
         subtask2.setStatus(TasksStatus.DONE);
         manager.updateSubTask(subtask2);
+        subtask4.setStatus(TasksStatus.DONE);
+        manager.updateSubTask(subtask4);
         System.out.println(manager.getSubTask(subtask2.getId()));
         subtask3.setStatus(TasksStatus.IN_PROGRESS);
         manager.updateSubTask(subtask3);
         System.out.println(manager.getSubTask(subtask3.getId()));
         System.out.println(manager.getEpicTask(epic1.getId()));
         System.out.println(manager.getEpicTask(epic2.getId()));
+        System.out.println(manager.getSubTask(subtask3.getId()));
+        System.out.println(manager.getSubTask(subtask4.getId()));
+        System.out.println(manager.getEpicTask(epic2.getId()));
+        System.out.println(manager.getEpicTask(epic3.getId()));
         System.out.println();
 
         System.out.println("Получение списка всех задач: ");
@@ -58,32 +71,42 @@ public class TaskTracker {
         System.out.println(manager.getAllSubtasks());
         System.out.println();
 
-        System.out.println("Удаление всех задач: ");
-        manager.clearTask();
-        System.out.println(manager.getAllTask());
-        System.out.println();
-
-        System.out.println("Удаление задачи task1: ");
-        manager.deleteTask(task1);
-        System.out.println(manager.getAllTask());
-        System.out.println();
-
-        System.out.println("Удаление подзадачи subtask2: ");
-        manager.deleteSubTask(subtask2);
-        System.out.println(manager.getAllSubtasks());
-        System.out.println();
-
-        System.out.println("Удаление эпика:");
-        manager.deleteEpicTask(epic2);
-        System.out.println(manager.getAllEpics());
-        System.out.println(manager.getAllSubtasks());
-
-        System.out.println("Удаление всех эпиков: ");
-        manager.clearEpicTask(epic1);
+//        System.out.println("Удаление всех задач: ");
+//        manager.clearTask();
+//        System.out.println(manager.getAllTask());
+//        System.out.println();
+//
+//        System.out.println("Удаление задачи task1: ");
+//        manager.deleteTask(task1);
+//        System.out.println(manager.getAllTask());
+//        System.out.println();
+//
+//        System.out.println("Удаление подзадачи subtask2: ");
+//        manager.deleteSubTask(subtask2);
+//        System.out.println(manager.getAllSubtasks());
+//        System.out.println();
+//
+//        System.out.println("Удаление эпика:");
+//        manager.deleteEpicTask(epic2);
+//        System.out.println(manager.getAllEpics());
+//        System.out.println(manager.getAllSubtasks());
+//
+//        System.out.println("Удаление всех эпиков: ");
+//        manager.clearEpicTask(epic1);
         System.out.println(manager.getAllEpics());
         System.out.println();
         System.out.println("Получение списка всех подзадач (подзадачи удаляются после удаления эпиков): ");
         System.out.println(manager.getAllSubtasks());
-        System.out.println("История просмотров" + manager.getHistoryManager().getHistory());
+        System.out.println("История просмотров:" + '\n' + manager.getHistoryManager().getHistory());
+     //   manager.getHistoryManager().remove(epic1.getId());
+        manager.deleteEpicTask(epic1);
+        System.out.println("***********************************************************************");
+        manager.getTask(task2.getId());
+       // manager.getEpicTask(epic2.getId());
+       // manager.getEpicTask(epic2.getId());
+        manager.getTask(task1.getId());
+     //   manager.getTask(task2.getId());
+        manager.getSubTask(subtask3.getId());
+        System.out.println("История просмотров:" + '\n' + manager.getHistoryManager().getHistory());
     }
 }
